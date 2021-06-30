1e79a0d8aa1cd0bf3f4cab522e41ecafd78e2fa5
Abas101 <35997611+Abas101@users.noreply.github.com>
2021-06-30 20:51:41 +0400

    Delete saba-kikvidze-davaleba5 directory

--- saba-kikvidze-davaleba5/src/main/java/saba/kikvidze/davaleba5/aspect/Print.java
+++
@@ -1,4 +0,0 @@
-package saba.kikvidze.davaleba5.aspect;
-
-public @interface Print {
-}
--- saba-kikvidze-davaleba5/src/main/java/saba/kikvidze/davaleba5/service/CalculatorService.java
+++
@@ -1,6 +0,0 @@
-package saba.kikvidze.davaleba5.service;
-
-public interface CalculatorService {
-
-    int multiply(int x, int y);
-}
